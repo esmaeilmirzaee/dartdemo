47911d7bdabb002807cba76aff8a0468d5ab82e1
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2023-06-13 19:06:18 +0330

    Use generator to access data

--- bin/main.dart
+++ bin/main.dart
@@ -2,9 +2,27 @@ import 'package:http/http.dart' show Response;
 import 'package:dartdemo/dartdemo.dart';
 import 'package:dartdemo/utils.dart';
 
-void main(List<String> arguments) async {
+List<Account> _accounts = [];
+
+initState() async {
   Response response = await fetchHeavyData();
   String res = response.body;
-  final githubs = accountFromJson(res);
-  print(githubs.length);
+  _accounts = accountFromJson(res);
+}
+
+Stream<Account> getInfoRange(int start, int finish) async* {
+  if (start <= finish) {
+    yield (_accounts[start]);
+    yield* getInfoRange(start + 1, finish);
+  }
+}
+
+printAccount(Account account) {
+  print('id: ${account.id}\n Created at: ${account.createdAt}');
+}
+
+void main(List<String> arguments) async {
+  await initState();
+  getInfoRange(1, 3).forEach(printAccount);
+  print(_accounts.length);
 }
